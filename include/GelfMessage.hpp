26babedc0521985fefa5de5da85cc71d8dd2325e
Steven Bidny <sbidny@gelbergroup.com>
2012-06-22 18:14:27 -0500

    Added JSON Spirit
    Added json_spirit directly to this project, since it is header-only.

--- include/GelfMessage.hpp
+++ include/GelfMessage.hpp
@@ -20,7 +20,7 @@
 
 // Third-party Header Files
 
-#include <json_spirit_writer_template.h>
+#include "json_spirit/json_spirit_writer_template.h"
 #include <boost/iostreams/filtering_stream.hpp>
 #include <boost/iostreams/copy.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
